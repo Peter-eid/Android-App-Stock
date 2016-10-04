93fa62f9069890f2fd36f5439357ae545c22c3b5
Peter <peter_eid@live.com>
2016-10-04 22:07:07 +0300

    added all api calls.

--- StockApp/app/src/main/java/com/grizly/newposapp/connectivity/MyApiEndpointInterface.java
+++ StockApp/app/src/main/java/com/grizly/newposapp/connectivity/MyApiEndpointInterface.java
@@ -2,6 +2,8 @@ package com.grizly.newposapp.connectivity;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Path;
+import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.POST;
 
@@ -13,5 +15,23 @@ public interface MyApiEndpointInterface {
 
     @POST("/login")
     Call<String> login(@Body String body);
+
+    @POST("/user")
+    Call<String> addUser(@Body String body);
+
+    @DELETE("/user/{id}")
+    Call<String> deleteUser(@Path("id") String id);
+
+    @POST("/product")
+    Call<String> addProduct(@Body String body);
+
+    @DELETE("/product/{id}")
+    Call<String> deleteProduct(@Path("id") String id);
+
+    @POST("/operation")
+    Call<String> addOperation(@Body String body);
+
+    @DELETE("/product/{id}")
+    Call<String> deleteOperation(@Path("id") String id);
 
 }
