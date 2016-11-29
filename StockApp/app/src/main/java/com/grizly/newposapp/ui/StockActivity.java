d00c125d6e7e3938629e5fdeb648233b5d7303b0
Peter <peter_eid@live.com>
2016-11-29 15:51:51 +0200

    added logout button.

--- StockApp/app/src/main/java/com/grizly/newposapp/ui/StockActivity.java
+++ StockApp/app/src/main/java/com/grizly/newposapp/ui/StockActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -70,6 +73,27 @@ public class StockActivity extends AppCompatActivity {
 
     public ImageView getMore_iv() {
         return (ImageView) findViewById(R.id.more);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_logout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.logout_action:
+                Intent intent = new Intent(StockActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
